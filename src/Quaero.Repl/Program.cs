1b818dbcf6d7d4cd091e70dd808c575b15859221
Kristian Hellang <kristian@hellang.com>
2023-06-07 00:31:15 +0200

    Add quick test REPL

---
+++ src/Quaero.Repl/Program.cs
@@ -0,0 +1,19 @@
+﻿using Quaero;
+
+Console.Write("query> ");
+var line = Console.ReadLine();
+
+while (line != null)
+{
+    if (!string.IsNullOrEmpty(line))
+    {
+        var filter = Filter.Parse(line).Optimize();
+
+        Console.WriteLine($"Graph: {filter.ToMicrosoftGraphQuery()}");
+        Console.WriteLine($"LDAP: {filter.ToLdapQuery()}");
+    }
+
+    Console.WriteLine();
+    Console.Write("query> ");
+    line = Console.ReadLine();
+}
