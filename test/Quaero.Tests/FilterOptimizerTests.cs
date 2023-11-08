2173bc6b87a69862b5dbba4ad0b7feb1c82693c1
Kristian Hellang <kristian@hellang.com>
2023-11-08 15:28:55 +0100

    Add test for filter optimizer

---
+++ test/Quaero.Tests/FilterOptimizerTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Quaero.Tests
+{
+    public class FilterOptimizerTests
+    {
+        [Fact]
+        public void ShouldNormalizeDoubleNegation()
+        {
+            var filter = Not(Not(StartsWith("name", "John"))).Optimize();
+            Assert.Equal(StartsWith("name", "John"), filter);
+        }
+
+        [Fact]
+        public void ShouldNormalizeNotEqual()
+        {
+            var filter = Not(Equal("test", 42)).Optimize();
+            Assert.Equal(NotEqual("test", 42), filter);
+        }
+
+        [Fact]
+        public void ShouldNormalizeComparisonOperators()
+        {
+            var filter = Not(GreaterThan("test", 42)).Optimize();
+            Assert.Equal(LessThanOrEqual("test", 42), filter);
+
+            filter = Not(GreaterThanOrEqual("test", 42)).Optimize();
+            Assert.Equal(LessThan("test", 42), filter);
+
+            filter = Not(LessThan("test", 42)).Optimize();
+            Assert.Equal(GreaterThanOrEqual("test", 42), filter);
+
+            filter = Not(LessThanOrEqual("test", 42)).Optimize();
+            Assert.Equal(GreaterThan("test", 42), filter);
+        }
+    }
+}
