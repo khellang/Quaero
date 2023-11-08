b716243284217be06a66dce04e97ada2cc580053
Kristian Hellang <kristian@hellang.com>
2023-11-08 15:29:38 +0100

    Implement equality for PropertyFilter

--- src/Quaero/PropertyFilter.cs
+++ src/Quaero/PropertyFilter.cs
@@ -4,7 +4,7 @@ namespace Quaero;
 /// A base class for filters operating on a property and its value.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public abstract class PropertyFilter<T> : Filter
+public abstract class PropertyFilter<T> : Filter, IEquatable<PropertyFilter<T>?>
 {
     /// <summary>
     /// Constructs a new instance of a <see cref="PropertyFilter{T}"/>.
@@ -22,4 +22,16 @@ public abstract class PropertyFilter<T> : Filter
     /// The value to check for.
     /// </summary>
     public T Value { get; }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => Equals(obj as PropertyFilter<T>);
+
+    /// <inheritdoc/>
+    public bool Equals(PropertyFilter<T>? other) =>
+        other is not null &&
+            StringComparer.OrdinalIgnoreCase.Equals(Name, other.Name) &&
+               EqualityComparer<T>.Default.Equals(Value, other.Value);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => HashCode.Combine(StringComparer.OrdinalIgnoreCase.GetHashCode(Name), Value);
 }
