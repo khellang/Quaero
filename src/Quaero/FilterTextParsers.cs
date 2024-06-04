160d1efdb481dc8a002600ae33e395232e682e63
Kristian Hellang <kristian@hellang.com>
2024-06-04 23:22:56 +0200

    Utilize ReadOnlySpan<char>-based parsing

--- src/Quaero/FilterTextParsers.cs
+++ src/Quaero/FilterTextParsers.cs
@@ -9,5 +9,9 @@ internal static class FilterTextParsers
         from open in Character.EqualTo('\"')
         from content in Character.HexDigit.Or(Character.EqualTo('-')).Many()
         from close in Character.EqualTo('\"')
+#if NET8_0_OR_GREATER
+        select System.Guid.Parse(content);
+#else
         select System.Guid.Parse(new string(content));
+#endif
 }
